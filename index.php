099d51b1d1bacad2c7f5bf6684e48484677e95fa
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-07 02:40:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
 <?php
-$servername='';
-$username='Admin';
-$password='raspberry';
-$dbname = "sensor_data";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-else
-{
-echo "connect";
-}
+echo "hello world";
 
 ?>
