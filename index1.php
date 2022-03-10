70cb2b73a5dfcb4def2f7b933ad770cba63b0177
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-10 17:10:42 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,3 +1,14 @@
 <?php
-echo "welcome";
+$servername = "remotemysql.com";
+$username = "g8S5n3brih";
+$password = "mxyceqDezM";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
