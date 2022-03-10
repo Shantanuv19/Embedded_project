bfc4ff546a195636958ffac3052fb83482f09ae1
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-10 17:40:02 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -12,13 +12,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-$sql = "SELECT id, name, branch FROM table";
+$sql = "SELECT id, name, branch, reg FROM table";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["branch"]. "<br>";
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. "branch- " . $row["branch"]. " " .$row["reg"]. "<br>";
   }
 } else {
   echo "0 results";
