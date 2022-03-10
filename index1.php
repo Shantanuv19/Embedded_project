397caf6a0e6c26292cdef35a848af2a3d3436083
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-10 17:32:34 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -14,18 +14,5 @@ else
 {
 echo "connected";
 
-$sql = "SELECT id, name, branch, reg FROM g8S5n3brih";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["branch"]. "<br>";
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-
 $conn = null;
 ?>
