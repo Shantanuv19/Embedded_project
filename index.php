cbd118b5a4baa78b909e17ceba080e301ff7995d
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-12 12:08:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,33 +1,20 @@
+
 <?php
-if(isset($_GET['id'])) {
-$id = $_GET['id'];
-echo file_get_contents('https://drive.google.com/u/4/uc?id='.$id.'&export=download');
-}   else {
-    echo '<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Drive to Web Hosting</title>
-    <script src="https://bit.ly/drivetoweb2-anly"></script>
-</head>
-<body>
-    <form action="" method="get">
-     <input type="text" placeholder="Enter File ID " name="id">
-     <button type="submit">View</button>
-    </form>
-    <style>
-    input , button {
-        background : aqua ;
-        font-size : 25px ;
-        border : none ;
-        outline : none ;
-        padding : 4px ;
-    }
-    </style>
-</body>
-</html>';
+$servername = "10.36.252.76";
+$username = "rootnew";
+$password = "00000";
+//$dbname = "g8S5n3brih";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
 }
-
+else
+{
+echo "Connected successfully";
+}
+$conn->close();
 ?>
