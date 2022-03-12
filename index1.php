c451b01a7be8cfae0e26a9021d4b5d8050674b8b
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-12 12:05:22 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "remotemysql.com";
-$username = "g8S5n3brih";
-$password = "mxyceqDezM";
-$dbname = "g8S5n3brih";
+$servername = "10.36.252.76";
+$username = "rootnew";
+$password = "00000";
+//$dbname = "g8S5n3brih";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
@@ -11,17 +11,9 @@ $conn = new mysqli($servername, $username, $password);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
+else
+{
 echo "Connected successfully";
-$sql = "SELECT id, name, branch, reg FROM table";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["name"]. "branch- " . $row["branch"]. " " .$row["reg"]. "<br>";
-  }
-} else {
-  echo "0 results";
 }
 $conn->close();
 ?>
