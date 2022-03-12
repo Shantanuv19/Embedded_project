48c9242918a1f4c0721f367daa7d1f9d41aacaea
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-12 12:13:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,18 +3,16 @@
 $servername = "http://10.36.252.76/";
 $username = "rootnew";
 $password = "00000";
-//$dbname = "g8S5n3brih";
+$dbname = "test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 else
-{
 echo "Connected successfully";
-}
 $conn->close();
 ?>
