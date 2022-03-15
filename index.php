98c0a64b58292ef5267194202d9fcaed98614ffe
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-15 19:57:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 
 <?php
-$servername = "https://a158-223-231-233-68.ngrok.io";
+$servername = "http://a158-223-231-233-68.ngrok.io";
 $username = "root";
 $password = "root";
 $dbname = "rfidattendance";
