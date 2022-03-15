6ae3bae44303afabb4ec943446d2e5bc5e1c2067
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-03-15 20:16:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,17 @@
-
 <?php
-$servername = "https://a158-223-231-233-68.ngrok.io";
-$username = "root";
-$password = "";
+$servername='https://a158-223-231-233-68.ngrok.io/';
+$username='root';
+$password='';
 $dbname = "rfidattendance";
-
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 else
 {
-echo "Connected successfully";
+    echo "connected";
 }
-$conn->close();
 ?>
