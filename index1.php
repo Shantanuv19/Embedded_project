6d14707a7e8f9b00feae8cbd5e9b17e073608f0d
Shantanu kumar <96616455+Shantanuv19@users.noreply.github.com>
2022-04-22 18:10:15 +0530

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,7 +1,7 @@
 <?php
-$servername = "10.36.252.76";
-$username = "rootnew";
-$password = "123456789";
+$servername = "remotemysql.com:3306";
+$username = "g8S5n3brih";
+$password = "9VOy8poxJ6";
 //$dbname = "g8S5n3brih";
 
 // Create connection
